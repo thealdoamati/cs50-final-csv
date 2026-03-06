40b3bba3dc420867e3b3b00bda3b0c10bbc94a15
Aldo Amati <aldilo08@hotmail.com>
2026-03-06 09:13:00 -0300

    new: inputs and ignore

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("uploaded.html")
     
