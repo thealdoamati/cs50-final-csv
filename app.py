4ec00f50406b305354f10b6f2a9ac3405c8ccccb
Aldo Amati <aldilo08@hotmail.com>
2026-03-09 11:02:58 -0300

    new: reading csv

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import os
+import csv
 
 app = Flask(__name__)
 
@@ -9,10 +11,23 @@ def index():
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
-        file = request.form.get("file")
+        file = request.files.get("file")
 
         if not file:
             return render_template("index.html")
+        
+        _, extension = os.path.splitext(file.filename)
+
+        if extension != ".csv":
+            print("Only csv")
+            return render_template("index.html")
+        
+        reader = csv.reader(file.stream.read().decode("utf-8").splitlines())
+        rows = []
+        for row in reader:
+            rows.append(row)
+
+        header = rows[0]
         
         print(f"file: {file}")
         return render_template("upload.html")
